7c11ba8ceb6505c3ee7a09cf0be7ff1ba42f836f
Dsun91 <dialsunny91@gmail.com>
2022-10-02 14:19:51 +0100

    added EI and UCB acquisition functions

--- src/EnsemblesOpt/Bayesian.py
+++ src/EnsemblesOpt/Bayesian.py
@@ -35,9 +35,10 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 class Bayesian_Voting_Ensemble:
     
-    def __init__(self,ensemble_size,models_list,xi,random_init_points,scoring,maximize_obj,task,type_p='soft'):
+    def __init__(self,ensemble_size,models_list,xi,random_init_points,scoring,maximize_obj,task,type_p='soft',acquisition_func='PI'):
+        self.acquisition_func=acquisition_func
         self.size_problem=ensemble_size
-        self.models_lists=models_list
+        self.models_list=models_list
         self.xi=xi
         self.random_init_points=random_init_points
         self.maximize_obj=maximize_obj
@@ -50,6 +51,8 @@ class Bayesian_Voting_Ensemble:
         self.points_vs=dict()
         self.counter=0
         self.type_p=type_p
+        if self.acquisition_func=='UCB':
+            self.lmda=float(input('select lambda value: '))
         self.Regressors=[
              ExtraTreeRegressor(),
              DecisionTreeRegressor(),
@@ -88,76 +91,13 @@ class Bayesian_Voting_Ensemble:
              LGBMClassifier(),
              CatBoostClassifier()]
         
-    def fit(self,X_train,y_train,n_iters,Nfold,stratify=False):
-        self.X_train=X_train
-        self.y_train=y_train
-        self.stratify=stratify
-        self.Nfold=Nfold
-        init_points=[]
-        
-       
-      
-        for i in range(self.size_problem):
-            Xi=np.random.randint(0,self.space_dim,self.random_init_points)
-            init_points.append(Xi)
-
-        print('Collecting initial random points...')
-        y=self.objective(init_points)
-        print('Searching best ensemble...')
-        X=pd.DataFrame(init_points).transpose()
-
-
-        #sns.relplot(init_points[0],init_points[1])
-        #plt.show()
-        y = np.asarray(y).reshape(len(y), 1)
-
-        #print(y)
-
-        model = GaussianProcessRegressor()
-        #model=RandomForestRegressor()
-        model.fit(X, y)
-
-        for i in range(10000):
-            
-            x = self.opt_acquisition(X, y, model,size_problem=self.size_problem,maximize=self.maximize_obj,xi=self.xi)
-
-            next_p=[np.round(j) for j in x]
-            if next_p not in self.points_done:
-                actual = self.objective(next_p)
-                self.points_done.append(next_p)
-                self.points_vs[tuple(next_p)]=actual
-                print('-trial ',self.counter,'|Score value:', actual[0])
-                self.counter+=1
-            else:
-                actual=self.points_vs[tuple(next_p)]
-               
-            est, _ = self.surrogate(model, [next_p])
-            
-        
-            X = vstack((X, [x]))          
-            y = vstack((y, [[actual[0]]]))
-           
-            model.fit(X, y)
-            if self.counter==n_iters:
-                break
-
-        if self.size_problem==2:
-            self.plot(model)
-            sns.relplot(X[:,0],X[:,1],hue=[x[0] for x in y]).set(title='Search Points')
-            plt.show() 
-        if self.maximize_obj==True:
-            ix = np.argmax(y)
-        else:
-            ix = np.argmin(y)
-        best_ensemble=[self.db[np.round(i)] for i in X[ix]]
-        print('Best Ensemble:\n',best_ensemble,'\nbest score',y[ix][0])
-        return best_ensemble
+    
 
     
     def load_dict_models(self,list_models):
         if len(list_models)>0:
             for i in range(len(list_models)):
-                self.db[i]=self.list_models[i]
+                self.db[i]=list_models[i]
         else:
             if self.task=='classification':
                 for i in range(len(self.Classifiers)):
@@ -197,17 +137,54 @@ class Bayesian_Voting_Ensemble:
             simplefilter("ignore")
             return model.predict(X, return_std=True)
     
-    def acquisition(self,X, Xsamples, model,xi):
+    def acquisition(self,X, Xsamples, model):
+        
+
+        if self.acquisition_func=='PI':
+            yhat, _ = self.surrogate(model, X)
+            if self.maximize_obj==True:
+                best = max(yhat)
+            else:
+                best = min(yhat)
+            
+            mu, std = self.surrogate(model, Xsamples)
+            #print(mu,best)
+            with np.errstate(divide='warn'):
+                probs = norm.cdf((mu - best - self.xi) / (std))
+
+        elif self.acquisition_func=='EI':
+            yhat, _ = self.surrogate(model, X)
+            
+            if self.maximize_obj==True:
+                best = max(yhat)
+            else:
+                best = min(yhat)
+            
+            mu, std = self.surrogate(model, Xsamples)
+        
+            PHI_Z=norm.cdf((mu - best - self.xi) / (std))
+            phi_Z=norm.pdf((mu - best - self.xi) / (std))
+            with np.errstate(divide='warn'):
+                probs=(mu - best - self.xi)*PHI_Z+std*phi_Z
+        
+        elif self.acquisition_func=='UCB':
+            
+            yhat, _ = self.surrogate(model, X)
+        
+            if self.maximize_obj==True:
+                best = max(yhat)
+            else:
+                best = min(yhat)
+            
+            mu, std = self.surrogate(model, Xsamples)
     
-        yhat, _ = self.surrogate(model, X)
-        best = min(yhat)
-        mu, std = self.surrogate(model, Xsamples)
-        #print(mu,best)
-        probs = norm.cdf((mu - best - self.xi) / (std))
+            probs=mu + self.lmda*std
+        
+
         return probs
  
     # optimize the acquisition function
-    def opt_acquisition(self,X, y, model,size_problem=2,xi=0.01,maximize=True):
+    def opt_acquisition(self,X, y, model,size_problem=2):
         # random search, generate random samples
 
         Xsamples=[]
@@ -216,13 +193,78 @@ class Bayesian_Voting_Ensemble:
             Xsamples.append(Xi)
         Xsamples=pd.DataFrame(Xsamples).transpose()
 
-        scores = self.acquisition(X, Xsamples, model,xi)
-        if maximize==True:
+        scores = self.acquisition(X, Xsamples, model)
+        if self.maximize_obj==True:
             ix = np.argmax(scores)
         else:
             ix = np.argmin(scores)
 
         return Xsamples.iloc[ix]
+
+    def fit(self,X_train,y_train,n_iters,Nfold,stratify=False):
+        self.X_train=X_train
+        self.y_train=y_train
+        self.stratify=stratify
+        self.Nfold=Nfold
+        init_points=[]
+        
+       
+      
+        for i in range(self.size_problem):
+            Xi=np.random.randint(0,self.space_dim,self.random_init_points)
+            init_points.append(Xi)
+
+        print('Collecting initial random points...')
+        y=self.objective(init_points)
+        print('Searching best ensemble...')
+        X=pd.DataFrame(init_points).transpose()
+
+
+        #sns.relplot(init_points[0],init_points[1])
+        #plt.show()
+        y = np.asarray(y).reshape(len(y), 1)
+
+        #print(y)
+
+        model = GaussianProcessRegressor()
+        
+        model.fit(X, y)
+
+        for i in range(10000):
+            
+            x = self.opt_acquisition(X, y, model,size_problem=self.size_problem)
+
+            next_p=[np.round(j) for j in x]
+            if next_p not in self.points_done:
+                actual = self.objective(next_p)
+                self.points_done.append(next_p)
+                self.points_vs[tuple(next_p)]=actual
+                print('-trial ',self.counter,'|Score value:', actual[0])
+                self.counter+=1
+            else:
+                actual=self.points_vs[tuple(next_p)]
+               
+            #est, _ = self.surrogate(model, [next_p])
+            
+        
+            X = vstack((X, [x]))          
+            y = vstack((y, [[actual[0]]]))
+           
+            model.fit(X, y)
+            if self.counter==n_iters:
+                break
+
+        if self.size_problem==2:
+            self.plot(model)
+            sns.relplot(X[:,0],X[:,1],hue=[x[0] for x in y]).set(title='Search Points')
+            plt.show() 
+        if self.maximize_obj==True:
+            ix = np.argmax(y)
+        else:
+            ix = np.argmin(y)
+        best_ensemble=[self.db[np.round(i)] for i in X[ix]]
+        print('Best Ensemble:\n',best_ensemble,'\nbest score',y[ix][0])
+        return best_ensemble
 
     def plot(self,model):
         Xsamples=[]
